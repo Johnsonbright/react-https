d06bfd9a1c30ed9c9799a5ec40d51d89d947cde6
Uzoma Ifechukwu <johnsonbright101@gmail.com>
2024-08-24 19:43:02 +0100

    first commit

---
+++ react-http/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Postlist } from './components/Postlist';
+
+function App() {
+  return (
+    <div className="App">
+      <Postlist />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-http/src/components/Postlist.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {axios} from 'react'
+
+export const Postlist = () => {
+  return (
+    <div>Postlist</div>
+  )
+}
